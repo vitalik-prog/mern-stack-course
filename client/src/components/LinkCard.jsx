89cffdd125c3f7380e4b6ebef452a952f9e2dac8
Vitaliy <270vitalik270@gmail.com>
2021-10-20 13:48:23 +0300

    added clicks counter logic and redirect to link

--- client/src/components/LinkCard.jsx
+++ client/src/components/LinkCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const LinkCard = ({ link }) => {
+const LinkCard = ({ link, clicksCountHandler }) => {
   return (
     <>
      <h2>Link:</h2>
-     <p>Yours link: <a href={link.to} target='_blank' rel='noopener noreferrer'>{link.to}</a></p>
+     <p>Yours link: <a onClick={clicksCountHandler} href={link.to} target='_blank' rel='noopener noreferrer'>{link.to}</a></p>
      <p>From: <a href={link.from} target='_blank' rel='noopener noreferrer'>{link.from}</a></p>
      <p>Clicks count: <strong>{link.clicks}</strong></p>
      <p>Created date: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
--- client/src/pages/DetailPage.jsx
+++ client/src/pages/DetailPage.jsx
@@ -8,6 +8,7 @@ import LinkCard from "../components/LinkCard";
 
 const DetailPage = () => {
   const [link, setLink] = useState(null)
+  const [isLinkClicked, setIsLinkClicked] = useState(false)
   const linkId = useParams().id
   const {request, loading} = useHttp()
   const auth = useContext(AuthContext)
@@ -17,20 +18,25 @@ const DetailPage = () => {
         Authorization: `Bearer ${auth.token}`
       })
       setLink(fetched)
+      setIsLinkClicked(false)
     } catch (e) {}
   },[linkId, request, auth.token])
 
   useEffect(() => {
     getLink()
-  }, [getLink])
+  }, [getLink, isLinkClicked])
 
   if (loading) {
     return <Loader />
   }
 
+  const clicksCountHandler = () => {
+    setIsLinkClicked(true)
+  }
+
   return (
     <>
-      {!loading && link && <LinkCard link={link} />}
+      {!loading && link && <LinkCard link={link} clicksCountHandler={clicksCountHandler} />}
     </>
   );
 };
