2ed3b727e3fb66e42e33fa6f939bc3b1b22f8ea3
Vitaliy <270vitalik270@gmail.com>
2021-10-21 10:39:55 +0300

    changed app.js for deployment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
 
-const PORT = config.get('port') || 5000
+const PORT = process.env.PORT || config.get('port') || 5000
 
 async function serverStart() {
   try {
